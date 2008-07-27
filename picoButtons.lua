c0586aff5c6a8a15cef4fd09ebfa3c3343441df2
Tekkub <tekkub@gmail.com>
2008-07-26 20:17:41 -0600

    And so it begins...

---
+++ picoButtons.lua
@@ -0,0 +1,33 @@
+
+local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
+local microcoord = {0, 1, 22/64, 1}
+
+
+local i = 0
+local function helper(title, button, label, icon)
+	ldb:NewDataObject(i.. " MicroButton "..title, {
+		type = "launcher",
+		icon = icon or "Interface\\Buttons\\UI-MicroButton-"..title.."-Up",
+		texcoord = not icon and microcoord,
+		OnClick = button:GetScript("OnClick"),
+		label = label,
+	})
+	i = i + 1
+end
+
+
+helper("Character", CharacterMicroButton, CHARACTER_BUTTON, "Interface\\GroupFrame\\UI-Group-LeaderIcon")
+helper("Spellbook", SpellbookMicroButton, SPELLBOOK_ABILITIES_BUTTON)
+helper("Talents", TalentMicroButton, TALENTS_BUTTON)
+helper("Quest", QuestLogMicroButton, QUESTLOG_BUTTON)
+helper("Socials", SocialsMicroButton, SOCIAL_BUTTON)
+helper("LFG", LFGMicroButton, LFG_BUTTON)
+helper("MainMenu", MainMenuMicroButton, MAINMENU_BUTTON)
+helper("Help", HelpMicroButton, HELP_BUTTON)
+
+
+---------------------------
+--      About panel      --
+---------------------------
+
+local about = LibStub("tekKonfig-AboutPanel").new(nil, "picoButtons")
