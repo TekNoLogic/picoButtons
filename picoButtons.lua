661071f63a4f83f4a9f8645a70ca1cb920afd9e4
Tekkub <tekkub@gmail.com>
2008-08-10 22:01:31 -0600

    Add PvP button

--- picoButtons.lua
+++ picoButtons.lua
@@ -4,12 +4,12 @@ local microcoord = {0, 1, 22/64, 1}
 
 
 local i = 0
-local function helper(title, button, label, icon)
+local function helper(title, button, label, icon, coords, click)
 	ldb:NewDataObject(i.. " MicroButton "..title, {
 		type = "launcher",
 		icon = icon or "Interface\\Buttons\\UI-MicroButton-"..title.."-Up",
-		texcoord = not icon and microcoord,
-		OnClick = button:GetScript("OnClick"),
+		texcoord = coords or (not icon and microcoord),
+		OnClick = click or button:GetScript("OnClick"),
 		label = label,
 	})
 	i = i + 1
@@ -22,6 +22,7 @@ helper("Talents", TalentMicroButton, TALENTS_BUTTON)
 helper("Achievement", AchievementMicroButton, ACHIEVEMENT_BUTTON)
 helper("Quest", QuestLogMicroButton, QUESTLOG_BUTTON)
 helper("Socials", SocialsMicroButton, SOCIAL_BUTTON)
+helper("PVP", PVPMicroButton, PLAYER_V_PLAYER, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8}, function() ToggleFrame(PVPFrame) end)
 helper("LFG", LFGMicroButton, LFG_BUTTON)
 helper("MainMenu", MainMenuMicroButton, MAINMENU_BUTTON)
 helper("Help", HelpMicroButton, HELP_BUTTON)
