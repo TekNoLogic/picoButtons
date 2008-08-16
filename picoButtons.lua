79dae7514a4f2634311cbac39d61bf5466d4fe37
Tekkub <tekkub@gmail.com>
2008-08-15 20:32:06 -0600

    Make compatible with both live and beta clients

--- picoButtons.lua
+++ picoButtons.lua
@@ -1,3 +1,4 @@
+if IS_WRATH_BUILD == nil then IS_WRATH_BUILD = (select(4, GetBuildInfo()) >= 30000) end
 
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local microcoord = {0, 1, 22/64, 1}
@@ -19,10 +20,10 @@ end
 helper("Character", CharacterMicroButton, CHARACTER_BUTTON, "Interface\\GroupFrame\\UI-Group-LeaderIcon")
 helper("Spellbook", SpellbookMicroButton, SPELLBOOK_ABILITIES_BUTTON)
 helper("Talents", TalentMicroButton, TALENTS_BUTTON)
-helper("Achievement", AchievementMicroButton, ACHIEVEMENT_BUTTON)
+if IS_WRATH_BUILD then helper("Achievement", AchievementMicroButton, ACHIEVEMENT_BUTTON) end
 helper("Quest", QuestLogMicroButton, QUESTLOG_BUTTON)
 helper("Socials", SocialsMicroButton, SOCIAL_BUTTON)
-helper("PVP", PVPMicroButton, PLAYER_V_PLAYER, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8}, function() ToggleFrame(PVPFrame) end)
+if IS_WRATH_BUILD then helper("PVP", PVPMicroButton, PLAYER_V_PLAYER, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8}, function() ToggleFrame(PVPFrame) end) end
 helper("LFG", LFGMicroButton, LFG_BUTTON)
 helper("MainMenu", MainMenuMicroButton, MAINMENU_BUTTON)
 helper("Help", HelpMicroButton, HELP_BUTTON)
