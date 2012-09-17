9e6cd3d8236edd5564756945e12e512c3e3991e7
tekkub :bear:  <tekkub@gmail.com>
2012-09-16 20:12:49 -0600

    Fix for 5.0

--- picoButtons.lua
+++ picoButtons.lua
@@ -8,32 +8,48 @@ local microcoord = {0, 1, 22/64, 1}
 
 local i = 0
 local function helper(title, button, label, icon, coords, click)
-	ldb:NewDataObject(i.. " MicroButton "..title, {
-		type = "launcher",
-		icon = icon or "Interface\\Buttons\\UI-MicroButton-"..title.."-Up",
-		texcoord = coords or (not icon and microcoord),
-		iconCoords = coords or (not icon and microcoord),
-		OnClick = click or button:GetScript("OnClick"),
-		label = label,
-	})
-	i = i + 1
+  ldb:NewDataObject(i.. " MicroButton "..title, {
+    type = "launcher",
+    icon = icon or "Interface\\Buttons\\UI-MicroButton-"..title.."-Up",
+    texcoord = coords or (not icon and microcoord),
+    iconCoords = coords or (not icon and microcoord),
+    OnClick = click or button:GetScript("OnClick"),
+    label = label,
+  })
+  i = i + 1
 end
 
 
 local x = MainMenuMicroButton:GetScript("OnMouseUp")
 local function mainmenu(self, ...) self.down = 1; x(self, ...) end
 
-helper("Character", CharacterMicroButton, CHARACTER_BUTTON, "Interface\\GroupFrame\\UI-Group-LeaderIcon", nil, function() ToggleCharacter("PaperDollFrame") end)
+
+helper("Character", CharacterMicroButton, CHARACTER_BUTTON,
+       "Interface\\GroupFrame\\UI-Group-LeaderIcon", nil,
+       function() ToggleCharacter("PaperDollFrame") end)
+
 helper("Spellbook", SpellbookMicroButton, SPELLBOOK_ABILITIES_BUTTON)
+
 helper("Talents", TalentMicroButton, TALENTS_BUTTON)
+
 helper("Achievement", AchievementMicroButton, ACHIEVEMENT_BUTTON)
+
 helper("Quest", QuestLogMicroButton, QUESTLOG_BUTTON)
-helper("Guild", GuildMicroButton, GUILD, "Interface\\Buttons\\UI-MicroButton-Socials-Up", microcoord)
-helper("PVP", PVPMicroButton, PLAYER_V_PLAYER, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8}, TogglePVPFrame)
-helper("LFD", LFDMicroButton, DUNGEONS_BUTTON, "Interface\\Buttons\\UI-MicroButton-LFG-Up", microcoord)
+
+helper("Guild", GuildMicroButton, GUILD,
+       "Interface\\Buttons\\UI-MicroButton-Socials-Up", microcoord)
+
+helper("PVP", PVPMicroButton, PLAYER_V_PLAYER,
+       "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"),
+       {0, 5/8, 0, 5/8}, TogglePVPFrame)
+
+helper("LFD", LFDMicroButton, DUNGEONS_BUTTON,
+       "Interface\\Buttons\\UI-MicroButton-LFG-Up", microcoord)
 
 helper("EJ", EJMicroButton, ENCOUNTER_JOURNAL)
-helper("Raid", RaidMicroButton, RAID)
+
+helper("Mounts", CompanionsMicroButton, MOUNTS_AND_PETS)
 
 helper("MainMenu", MainMenuMicroButton, MAINMENU_BUTTON, nil, nil, mainmenu)
+
 helper("Help", HelpMicroButton, HELP_BUTTON)
